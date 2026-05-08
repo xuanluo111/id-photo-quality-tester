517a28e32ad64e38acae6b4d8a699734a68655f1
xuanluo111 <xuanluo111@126.com>
2026-05-08 17:09:46 +0800

    push

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-    testDir: './e2e-tests/tests',
+    testDir: './tests',
     timeout: 30000,
     retries: 1,
     use: {
